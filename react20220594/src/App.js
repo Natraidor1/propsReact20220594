ca439da91733c595267d83f5a4a9378a9361834a
Natraidor <20220594@ricaldone.edu.sv>
2025-03-18 13:58:05 -0600

    todos los componentes terminado

--- react20220594/src/App.js
+++ react20220594/src/App.js
@@ -7,6 +7,19 @@ import Notification from './components/Notification';
 import BreadCrumb from './components/BreadCrumb';
 import ButtonGroup from './components/ButtonGroup';
 import Cards from './components/Cards';
+import CloseButton from './components/CloseButton';
+import Dropdown from './components/Dropdown';
+import ListGroup from './components/ListGroup';
+import Modal from './components/Modal';
+import Navbar from './components/Navbar';
+import NavTar from './components/NavTar';
+import Pagination from './components/Pagination';
+import Placeholder from './components/Placeholder';
+import Popovers from './components/Popovers';
+import Progress from './components/Progress';
+import Scrollspy from './components/Scrolspy';
+import Spinner from './components/Spinner';
+import Toast from './components/Toast';
 
 function App() {
   return (
@@ -18,8 +31,22 @@ function App() {
       <BreadCrumb />
       <ButtonGroup />
       <Cards />
+      <CloseButton />
+      <Dropdown />
+      <ListGroup />
+      <Modal />
+      <Navbar />
+      <NavTar />
+      <Pagination />
+      <Placeholder />
+      <Popovers />
+      <Progress />
+      <Scrollspy />
+      <Spinner   />
+      <Toast />
     </>
   );
 }
+
 
 export default App;
